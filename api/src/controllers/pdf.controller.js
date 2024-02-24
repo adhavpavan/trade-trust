cc9c00f1e85718ea3b04b54f38e3d67c451e7727
yash-desai-naik <yash.desai.naik@gmail.com>
2024-02-24 07:46:06 +0530

    pdf upload to s3 after saving parsed data to db

--- api/src/controllers/pdf.controller.js
+++ api/src/controllers/pdf.controller.js
@@ -1,6 +1,8 @@
 const { extractTextFromPDF, extractAddresses, extractOrderDetails, extractProductDetails, parseHouseBillText, parseInvoiceText } = require('../services/pdf.service');
 const catchAsync = require('../utils/catchAsync');
 const fs = require('fs');
+const { uploadFile } = require('../utils/fileUpload');
+const { DeliveryProof , Invoice, Bill} = require('../models');
 
 
 const parseDeliveryProofPDF = catchAsync(async (req, res) => {
@@ -12,7 +14,7 @@ const parseDeliveryProofPDF = catchAsync(async (req, res) => {
   const greenCodeMatch = pdfText.match(/WH\/OUT\/\d{5}/);
   const greenCode = greenCodeMatch ? greenCodeMatch[0] : null;
 
-  const result = {
+  const data = {
     customerAddress,
     deliveryAddress,
     orderDetails,
@@ -20,11 +22,24 @@ const parseDeliveryProofPDF = catchAsync(async (req, res) => {
     greenCode,
   };
 
+  //save data to database
+  const result = await DeliveryProof.create(data);
+  //save pdf file to s3
+  const file = {
+    originalname: `DELIVERY_PROOF_${data.orderDetails.OrderId}_${result._id}.pdf`,
+    mimetype: 'application/pdf',
+    path: req.file.path,
+  };
+  const orgName = req.loggerInfo?.user.orgName?? 'default_org';
+  const fileMetadata = await uploadFile(file, orgName);
+  console.log(fileMetadata);
+
+
 
   // delete the file
   fs.unlinkSync(req.file.path);
 
-  res.json(result);
+  res.json({data, fileMetadata});
 });
 
 const parseInvoicePDF = catchAsync(async (req, res) => {
@@ -32,10 +47,23 @@ const parseInvoicePDF = catchAsync(async (req, res) => {
   const pdfText = await extractTextFromPDF(req.file.path);
   const data = parseInvoiceText(pdfText);
 
+  //save data to database
+  const result = await Invoice.create(data);
+
+  //save pdf file to s3
+  const file = {
+    originalname: `INVOICE_${data.invoice}_${result._id}.pdf`,
+    mimetype: 'application/pdf',
+    path: req.file.path,
+  };
+  const orgName = req.loggerInfo?.user.orgName?? 'default_org';
+  const fileMetadata = await uploadFile(file, orgName);
+  console.log(fileMetadata);
+
   //delete the file
   fs.unlinkSync(req.file.path);
 
-  res.json(data);
+  res.json({data, fileMetadata});
 
 })
 
@@ -43,9 +71,22 @@ const parseHouseBillPDF = catchAsync(async (req, res) => {
   const pdfText = await extractTextFromPDF(req.file.path);
   const data = parseHouseBillText(pdfText);
 
+  //save data to database
+  const result = await Bill.create(data);
+  //save pdf file to s3
+  const file = {
+    originalname: `HOUSE_BILL_${data.shipperReference}_${result._id}.pdf`,
+    mimetype: 'application/pdf',
+    path: req.file.path,
+  };
+  const orgName = req.loggerInfo?.user.orgName?? 'default_org';
+  const fileMetadata = await uploadFile(file, orgName);
+  console.log(fileMetadata);
+
+
   //delete the file
   fs.unlinkSync(req.file.path);
-  res.json(data);
+  res.json({data, fileMetadata});
 })
 
 
--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,2 +1,8 @@
 module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
+module.exports.Organization = require('./organization.model');
+module.exports.Lot = require('./lot.model');
+module.exports.Invoice = require('./invoice.model');
+module.exports.DeliveryProof = require('./deliveryProof.model');
+module.exports.Bill = require('./bill.model');
+
